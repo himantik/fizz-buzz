f5e5e923974f8319561e16a3991427cf0e27d3aa
himantik <himantik@yahoo.com>
2022-09-08 14:03:12 -0400

    committing

--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,14 +1,20 @@
 package com.tlglearning;
 
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) {
     int i = 1;
-    while( i <= 100 ){
-      System.out.println(FizzBuzz.valueOf(i));
-      i++;
+    do {
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
 
     }
+      System.out.println(FizzBuzz.valueOf(i));
+      System.out.printf("%d: %s%n", i value);
+      i++;
+
+    } while (i <= 100);
   }
 
 }
