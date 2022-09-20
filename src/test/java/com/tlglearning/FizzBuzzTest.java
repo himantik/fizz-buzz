72c0acbff3e2d4831dbff2e679ddde7ffdfa7f22
himantik <himantik@yahoo.com>
2022-09-20 14:03:51 -0400

    Fizz buzz assertion

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,6 +2,13 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 class FizzBuzzTest {
+  void valueOf(){
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
+
+
+  }
 
 }
